843a28ce30156d89f4afeef389c29c2a8e99c76b
Doggitoz <cpwheeler4@gmail.com>
2022-02-02 20:12:25 -0600

    Finished Prototype

---
+++ Apple Picker/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    [SerializeField] private GameObject basketPrefab;
+    [SerializeField] private int numBaskets = 3;
+    [SerializeField] private float basketBottomY = -14f;
+    [SerializeField] private float basketSpacingY = 2f;
+    [SerializeField] private List<GameObject> basketList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+    }
+
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene01");
+        }
+    }
+}
